e44eda3c951a9202e3dce6fc15d393872e732dc7
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-01-10 00:43:16 +0100

    Added TileTerrainFactory

---
+++ Map/Data/TileTerrainFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using GameOfLifeClans.Map.Enums;
+
+
+namespace GameOfLifeClans.Map.Data
+{
+    public class TileTerrainFactory
+    {
+        private TileTerrain[] _terrains;
+
+
+        public TileTerrain Terrain(TerrainId id) => _terrains[(int)id];
+
+
+        public TileTerrainFactory()
+        {
+            int count = Enum.GetNames(typeof(TerrainId)).Length;
+            _terrains = new TileTerrain[count];
+
+            _terrains[(int)TerrainId.Grass]     = new TileTerrain(TerrainId.Grass, true);
+            _terrains[(int)TerrainId.Desert]    = new TileTerrain(TerrainId.Desert, true);
+            _terrains[(int)TerrainId.Water]     = new TileTerrain(TerrainId.Water,    false);
+            _terrains[(int)TerrainId.Mountain]  = new TileTerrain(TerrainId.Mountain, false);
+
+            if (_terrains.Contains(null))
+            {
+                throw new Exception("_terrains[] in TileTerrainFactory contains NULL, not enough defined terrains");
+            }
+        }
+    }
+}
