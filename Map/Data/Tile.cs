1c2f75baeab9ec7639cf3afa1bd23e5f91448064
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-01-10 00:49:17 +0100

    Added some functions to Map.Data.Tile

--- Map/Data/Tile.cs
+++ Map/Data/Tile.cs
@@ -1,15 +1,20 @@
-﻿namespace GameOfLifeClans.Map.Data
+﻿using GameOfLifeClans.Ai;
+
+
+namespace GameOfLifeClans.Map.Data
 {
     public class Tile
     {
         public int LocationX { get; private set; }
         public int LocationY { get; private set; }
         public TileTerrain Terrain { get; private set; }
-        public Ai.Entity AiEntity { get; private set; }
+        public Entity AiEntity { get; private set; }
 
 
         public bool IsOccupied => !(AiEntity == null);
         public void SetTerrain(TileTerrain terrain) => Terrain = terrain;
+        public void SetAiEntity(Entity aiEntity) => AiEntity = aiEntity;
+        public void RemoveAiEntity() => AiEntity = null;
 
 
         public Tile(int x, int y, TileTerrain terrain)
@@ -17,6 +22,13 @@
             LocationX = x;
             LocationY = y;
             SetTerrain(terrain);
+        }
+
+
+        public void MoveAiEntityHere(Tile fromTile)
+        {
+            AiEntity = fromTile.AiEntity;
+            fromTile.RemoveAiEntity();
         }
     }
 }
