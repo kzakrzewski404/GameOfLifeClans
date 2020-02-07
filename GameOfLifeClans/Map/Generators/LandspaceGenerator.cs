4f1416d7898981879d1ee6f1afacdaa3663ee08e
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-07 23:14:08 +0100

    Added abstract landspaceGenerator

---
+++ GameOfLifeClans/Map/Generators/LandspaceGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GameOfLifeClans.Map;
+
+
+namespace GameOfLifeClans.Map.Generators
+{
+    public abstract class LandspaceGenerator
+    {
+        private static Random _rnd = new Random();
+        private bool[] _markedTerrainBuffor;
+
+        public abstract void Generate(MapContainer map);
+
+
+    }
+}
