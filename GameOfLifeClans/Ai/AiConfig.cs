99e7baf98f09d4005cae8d71519c57a5c4582521
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-08 17:43:32 +0100

    moved AiConfig to Ai\Config

--- GameOfLifeClans/Ai/AiConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameOfLifeClans.Ai
-{
-    public static class AiConfig
-    {
-        public const int HEADQUARTER_SPAWN_TRESHOLD = 15;
-
-        public const int SOLDIER_MINIMAL_WILLIGNESS_TO_ATTACK = 40;
-    }
-}
---
+++ GameOfLifeClans/Ai/Config/AiConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace GameOfLifeClans.Ai.Config
+{
+    public static class AiConfig
+    {
+        public const int HEADQUARTER_DAMAGE = 200;
+        public const int HEADQUARTER_HEALTH = 500;
+        public const int HEADQUARTER_DEFENCE = 50;
+        public const int HEADQUARTER_SPAWN_TRESHOLD = 15;
+
+        public const int SOLDIER_DAMAGE = 20;
+        public const int SOLDIER_HEALTH = 50;
+        public const int SOLDIER_DEFENCE = 0;
+        public const int SOLDIER_MINIMAL_WILLIGNESS_TO_ATTACK = 40;
+    }
+}
