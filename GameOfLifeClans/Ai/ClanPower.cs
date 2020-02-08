7e77696e7c92190b5c2082682f88e9ad89f2ed16
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-09 00:20:03 +0100

    Added ClanPower

---
+++ GameOfLifeClans/Ai/ClanPower.cs
@@ -0,0 +1,19 @@
+﻿namespace GameOfLifeClans.Ai
+{
+    public class ClanPower
+    {
+        public int ControlledTerritory {get; private set;}
+        public int DamageBonus => ControlledTerritory / 10;
+        public int DefenceBonus => ControlledTerritory / 25;
+
+
+        public ClanPower()
+        {
+            ControlledTerritory = 0;
+        }
+
+
+        public void LoseTerritory() => ControlledTerritory--;
+        public void GainTerritory() => ControlledTerritory++;
+    }
+}
