64daeaef21222054fcb6326015f83fdf3318dc76
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-10 03:22:37 +0100

    stylecop refactoring

--- GameOfLifeClans/Ai/ClanPower.cs
+++ GameOfLifeClans/Ai/ClanPower.cs
@@ -2,15 +2,15 @@
 {
     public class ClanPower
     {
-        public int ControlledTerritory {get; private set;}
-        public int DamageBonus => ControlledTerritory / 10;
-        public int DefenceBonus => ControlledTerritory / 25;
-
-
         public ClanPower()
         {
             ControlledTerritory = 0;
         }
+
+
+        public int ControlledTerritory {get; private set;}
+        public int DamageBonus => ControlledTerritory / 10;
+        public int DefenceBonus => ControlledTerritory / 25;
 
 
         public void LoseTerritory() => ControlledTerritory--;
