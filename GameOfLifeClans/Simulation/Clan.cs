5bc6ddacc83be245563367e98d22b10bc5f6ab23
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-10 01:04:07 +0100

    Added Clan class

---
+++ GameOfLifeClans/Simulation/Clan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GameOfLifeClans.Map.Data;
+using GameOfLifeClans.Ai.Enums;
+using GameOfLifeClans.Ai;
+
+
+namespace GameOfLifeClans.Simulation
+{
+    public class Clan
+    {
+        private ClanId _clanId;
+        private Headquarter _headquarter;
+        private List<Entity> _entitiesList = new List<Entity>();
+        private bool _isDestroyed;
+
+
+        public int EntitiesOnMap => _entitiesList.Count;
+        public bool IsAlive => !_isDestroyed;
+
+
+        public Clan(ClanId clanId, Tile spawnTile)
+        {
+            _clanId = clanId;
+            _entitiesList.Clear();
+            SpawnHeadquarter(spawnTile);
+            _isDestroyed = false;
+        }
+
+
+        public void CalculateStep()
+        {
+            foreach(Entity entity in _entitiesList)
+            {
+                entity.CalculateStep();
+            }
+        }
+
+
+        private void SpawnHeadquarter(Tile tile)
+        {
+            EntityFactory factory = new EntityFactory();
+            _headquarter = factory.Create(EntityId.Headquarter, _clanId) as Headquarter;
+            _entitiesList.Add(_headquarter);
+
+            tile.SetAiEntity(_headquarter);
+        }
+    }
+}
