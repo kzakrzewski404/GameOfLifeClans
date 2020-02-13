96853abb5bdfbf389c880eb7c49b12909c40b625
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-13 19:12:13 +0100

    Added EntityConfig and EntityConfigFactory

---
+++ GameOfLifeClans/Ai/Config/EntityConfig.cs
@@ -0,0 +1,22 @@
+﻿using GameOfLifeClans.Ai.Enums;
+
+
+namespace GameOfLifeClans.Ai.Config
+{
+    public class EntityConfig
+    {
+        public int Health { get; private set; }
+        public int Damage { get; private set; }
+        public int Defence { get; private set; }
+        public EntityId Id { get; private set; }
+
+
+        public EntityConfig(EntityId id, int healht, int damage, int defence)
+        {
+            Id = id;
+            Health = healht;
+            Damage = damage;
+            Defence = defence;
+        }
+    }
+}
