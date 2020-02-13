f760fb18384af70214a5d11c1f38ca47a9601135
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-13 21:05:22 +0100

    Splitted into separate configs

---
+++ GameOfLifeClans/Ai/Entities/Config/Behaviour.cs
@@ -0,0 +1,9 @@
+﻿namespace GameOfLifeClans.Ai.Entities.Config
+{
+    public static class Behaviour
+    {
+        public const int HEADQUARTER_SPAWN_TRESHOLD = 15;
+
+        public const int SOLDIER_MINIMAL_WILLIGNESS_TO_ATTACK = 40;
+    }
+}
