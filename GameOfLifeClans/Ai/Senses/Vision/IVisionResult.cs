72b66343acb8494ca9645f2851d8c294a7d56e27
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-13 21:09:45 +0100

    moved from old dir

---
+++ GameOfLifeClans/Ai/Senses/Vision/IVisionResult.cs
@@ -0,0 +1,21 @@
+﻿using GameOfLifeClans.Ai.Entities;
+using GameOfLifeClans.Map.Data;
+
+
+namespace GameOfLifeClans.Ai.Senses.Vision
+{
+    public interface IVisionResult
+    {
+        bool IsEnemyFound { get; }
+        bool IsAllyFound { get; }
+        bool IsFreeTileFound { get; }
+        int NumberOfEnemies { get; }
+        int NumberOfAllies { get; }
+        int NumberOfFreeTiles { get; }
+
+
+        IAttackable GetRandomEnemy();
+        Entity GetRandomAlly();
+        Tile GetRandomFreeTile();
+    }
+}
