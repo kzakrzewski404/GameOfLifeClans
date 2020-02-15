8b61fcf8c776cf96f7e7113a84a54622f4c27f06
kzakrzewski404 <59293105+kzakrzewski404@users.noreply.github.com>
2020-02-15 17:13:27 +0100

    Update EntityFactory.cs

--- GameOfLifeClans/Ai/Entities/EntityFactory.cs
+++ GameOfLifeClans/Ai/Entities/EntityFactory.cs
@@ -1,4 +1,5 @@
 ﻿using GameOfLifeClans.Ai.Data;
+using GameOfLifeClans.Ai.Entities.Config;
 using GameOfLifeClans.Ai.Enums;
 using GameOfLifeClans.Ai.Senses.Vision;
 using GameOfLifeClans.Simulation.Clan;
@@ -9,6 +10,7 @@ namespace GameOfLifeClans.Ai.Entities
     public class EntityFactory
     {
         private static IVisionSense _defaultVisionSense = new VisionOfSurrounding();
+        private static IVisionSense _builderVisionSense = new VisionOfBuilder(Behaviour.BUILDER_HEADQUARTER_CHECK_RANGE);
 
 
         public Entity Create(EntityId entityId, IClanInfo memberOfClan)
@@ -16,10 +18,16 @@ namespace GameOfLifeClans.Ai.Entities
             switch (entityId)
             {
                 case EntityId.Headquarter:
-                    return new Headquarter(memberOfClan, SpawnStatsFactory.Create(entityId), _defaultVisionSense);
+                    return new Headquarter(memberOfClan, SpawnStatsFactory.Create(entityId), _defaultVisionSense, Behaviour.HEADQUARTER_SPAWN_TRESHOLD);
+
+                case EntityId.Outpost:
+                    return new Outpost(memberOfClan, SpawnStatsFactory.Create(entityId), _defaultVisionSense, Behaviour.OUTPOST_SPAWN_TRESHOLD);
 
                 case EntityId.Soldier:
                     return new Soldier(memberOfClan, SpawnStatsFactory.Create(entityId), _defaultVisionSense);
+
+                case EntityId.Builder:
+                    return new Builder(memberOfClan, SpawnStatsFactory.Create(entityId), _builderVisionSense);
 
                 default:
                     throw new System.Exception("Missing entity in EntityFactory");
